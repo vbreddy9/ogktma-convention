be639769eb51a5da9fb563e8e8421f7d1a930499
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-06-17 22:47:13 +0530

    Update SponsorCarousel.jsx

--- frontend/src/components/SponsorCarousel.jsx
+++ frontend/src/components/SponsorCarousel.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/SponsorCarousel.css';
+import { Link } from 'react-router-dom';
+import { FaChair } from 'react-icons/fa'; // Font Awesome Chair icon
 import sponsor1 from '../assets/dummy-image.jpg';
 import sponsor2 from '../assets/dummy-image-1.jpg';
 import sponsor3 from '../assets/dummy-image-2.jpg';
@@ -12,8 +14,10 @@ const SponsorCarousel = () => {
       <div className="sponsor-wrapper">
         <div className="sponsor-left">
           <h2 className="sponsor-heading">Our Sponsors</h2>
-          <a href="#become-sponsor" className="sponsor-btn">
-            Become a Sponsor <span className="icon">👜</span>
+          <Link to="/register" className="register-btn">
+            <FaChair style={{ marginRight: '0.5rem' }} /> Become a Sponsor
+          </Link>
+            
           </a>
         </div>
         <div className="sponsor-right">
