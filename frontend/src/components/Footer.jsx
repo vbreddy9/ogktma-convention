40f2188da52992f0a853460bc42f3134094d39a3
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-07-08 11:20:30 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,13 +5,8 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© OGKTMA USA • info@ogktma.org • (630) 990‑1111</p>
-      <div className="social-icons">
-        <a href="#" aria-label="Facebook"><FaFacebookF /></a>
-        <a href="#" aria-label="Twitter"><FaTwitter /></a>
-        <a href="#" aria-label="Instagram"><FaInstagram /></a>
-        <a href="#" aria-label="YouTube"><FaYoutube /></a>
-      </div>
+      <p>© OGKTMA USA • info@ogktma.org </p>
+      
     </footer>
   );
 };
