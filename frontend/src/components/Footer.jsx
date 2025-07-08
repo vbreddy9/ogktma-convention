eaf73571582f739650ac5949a6807615978568e9
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-07-08 17:08:22 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,12 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© OGKTMA USA • info@ogktma.org </p>
+      <p>
+        © OGKTMA USA •{" "}
+        <a href="mailto:info@ogktma.org">info@ogktma.org</a> |
+        Email: <a href="mailto:ssandella@yahoo.com">ssandella@yahoo.com</a> |
+        Call: <a href="tel:+15023338929">502-333-8929</a>
+      </p>
       
     </footer>
   );
