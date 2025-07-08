5d10861d88c9e623ebfdce0d188b4f55b1d3e379
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-07-08 17:11:34 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,10 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>
-        © OGKTMA USA •{" "}
-        <a href="mailto:info@ogktma.org">info@ogktma.org</a> |
-        Email: <a href="mailto:ssandella@yahoo.com">ssandella@yahoo.com</a> |
-        Call: <a href="tel:+15023338929">502-333-8929</a>
+        © OGKTMA USA •{" "} | Email: <a href="mailto:ssandella@yahoo.com">ssandella@yahoo.com</a> | Call: <a href="tel:+15023338929">502-333-8929</a>
       </p>
       
     </footer>
