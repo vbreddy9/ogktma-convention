93434ab327d988317c637b754526769e7e0aad5d
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-07-08 11:22:55 +0530

    Update dignitaries.js

--- frontend/src/data/dignitaries.js
+++ frontend/src/data/dignitaries.js
@@ -1,9 +1,9 @@
-import dummy1 from '../assets/Dr_Chandra_Sekhar.jpg';
+
 import dummy2 from '../assets/Dr_Gadam_Vivekanand.jpg';
 
 
 export const dignitaries = [
-  { img: dummy1, name: "Dr. Chandra Sekhar Pemmasani", title: "Member of Parliament Guntur" },
+ 
   { img: dummy2, name: "Dr. Gaddam Vivekanand", title: "Labour and Mining Minister Telangana" },
     
 ];
