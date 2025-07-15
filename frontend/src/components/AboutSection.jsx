f7d16850499da070686c0966de0be62071be79b5
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-07-15 07:16:36 +0530

    Update AboutSection.jsx

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -6,7 +6,7 @@ const AboutSection = () => {
   return (
     <section id="about" className="about">
       <h2>About OGKTMA</h2>
-      <p>Founded in 1982, OGKTMA represents a conglomeration of more than 100,000 practicing physicians in the United States. 1 in every 7 people in the USA is touched by the care of a physician of Indian origin at any given time.</p>
+      <p>Founded in 1982, OGKTMA represents a conglomeration of more than 8,000 practicing physicians in the United States. 1 in every 7 people in the USA is touched by the care of a physician of Indian origin at any given time.</p>
       <a href="#packages" className="btn register-btn">
                 <FaChair style={{ marginRight: '0.5rem' }} /> Register Now
               </a>
