57ad0bed2782f0c719e4990d57f349f864b303b2
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-08-06 23:10:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,57 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Common Components
 import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Homepage Sections
 import Hero from './components/Hero';
 import AboutSection from './components/AboutSection';
-import DignitariesList from './components/DignitariesList';
-import Footer from './components/Footer';
 import InfoSection from './components/InfoSection';
+import AgendaComponent from './components/AgendaComponent';
+import DignitariesList from './components/DignitariesList';
 import SponsorCarousel from './components/SponsorCarousel';
 import EarlyBirdPackages from './components/EarlyBirdPackages';
 import CountdownSection from './components/CountdownSection';
-import AgendaComponent from './components/AgendaComponent';
 import VenueSection from './components/VenueSection';
+
+// Forms and Pages
 import MemberRegistrationForm from './components/MemberRegistrationForm.jsx';
-import SponsorshipSection from './components/SponsorshipSection';
+import SimpleRegistrationForm from './components/SimpleRegistrationForm.jsx';
+import ThankYou from './components/ThankYou.jsx';
+import ThankYouMember from './components/ThankYouMember.jsx'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <>
-            <Header />
-            <Hero />
-            <AgendaComponent />
-            <SponsorshipSection/>
-            <AboutSection />
-            <InfoSection />
-            <DignitariesList />
-            <SponsorCarousel />
-            <EarlyBirdPackages />
-            <CountdownSection />
-            <VenueSection />
-            <Footer />
-          </>
-        } />
-      
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Hero />
+              <AgendaComponent />
+              <AboutSection />
+              <InfoSection />
+              <DignitariesList />
+              <SponsorCarousel />
+              <EarlyBirdPackages />
+              <CountdownSection />
+              <VenueSection />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Registration Pages */}
         <Route path="/register" element={<MemberRegistrationForm />} />
+        <Route path="/become-sponsor" element={<SimpleRegistrationForm />} />
+
+        {/* Thank You Page */}
+        <Route path="/thank-you-member" element={<ThankYouMember />} />
+        <Route path="/thank-you" element={<ThankYou />} />
       </Routes>
     </Router>
   );
